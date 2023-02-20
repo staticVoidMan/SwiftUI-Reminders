2147af426464f2b2c72bb8c0e55a4ed3e3b8cb1b
staticVoidMan <staticVoidMan@users.noreply.github.com>
2023-02-20 12:36:59 +0530

    - Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Amin Siddiqui on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
