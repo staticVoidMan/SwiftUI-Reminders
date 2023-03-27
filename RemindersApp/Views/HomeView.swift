5be7116469a49cfc2dc004e3ec87b4e7b5abc4e8
staticVoidMan <staticVoidMan@users.noreply.github.com>
2023-03-27 11:14:24 +0530

    #23.1

--- RemindersApp/Views/HomeView.swift
+++ RemindersApp/Views/HomeView.swift
@@ -12,25 +12,39 @@ struct HomeView: View {
     @FetchRequest(sortDescriptors: [])
     private var reminderLists: FetchedResults<MyList>
     
+    @FetchRequest(sortDescriptors: [])
+    private var searchResult: FetchedResults<Reminder>
+    
     @State
     private var isPresented: Bool = false
     
+    @State
+    private var searchTerm: String = ""
+    
     var body: some View {
         NavigationStack {
-            ReminderListsView(lists: reminderLists)
-            
-            Spacer()
-            
-            Button("Add List") {
-                isPresented = true
+            VStack {
+                ReminderListsView(lists: reminderLists)
+                
+                Spacer()
+                
+                Button("Add List") {
+                    isPresented = true
+                }
+                .font(.headline)
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .trailing
+                )
+                .padding()
             }
-            .font(.headline)
-            .frame(
-                maxWidth: .infinity,
-                alignment: .trailing
-            )
-            .padding()
+            .overlay {
+                if !searchTerm.isEmpty {
+                    ReminderListView(reminders: searchResult)
+                }
+            }
         }
+        .searchable(text: $searchTerm)
         .sheet(isPresented: $isPresented) {
             AddNewListView(
                 onSave: { name, color in
